b29a15e043f56551bb0ad7424eaa5151edd1c45b
Matt Holmes <m4ttholmes@gmail.com>
2021-10-21 09:02:14 -0400

    Create New Logs complete

--- src/App.js
+++ src/App.js
@@ -1,28 +1,39 @@
 import React, {useState, useEffect} from "react";
 import Sitebar from "./home/Navbar";
 import Auth from "./auth/Auth";
+import WorkoutIndex from "./workouts/WorkoutIndex";
 
 function App() {
-  const [sessionToken, setSessionToken] = useState("");
+    const [sessionToken, setSessionToken] = useState("");
 
-  useEffect(() => {
-    if(localStorage.getItem("token")) {
-      setSessionToken(localStorage.getItem("token"));
+    useEffect(() => {
+        if(localStorage.getItem("token")) {
+            setSessionToken(localStorage.getItem("token"));
+        }
+    }, [])
+
+    const updateToken = (newToken) => {
+        localStorage.setItem("token", newToken);
+        setSessionToken(newToken);
+        console.log(sessionToken);
     }
-  }, [])
 
-  const updateToken = (newToken) => {
-    localStorage.setItem("token", newToken);
-    setSessionToken(newToken);
-    console.log(sessionToken);
-  }
+    const clearToken = () => {
+        localStorage.clear();
+        setSessionToken("");
+    }
 
-  return (
-    <div>
-      <Sitebar />
-      <Auth updateToken={updateToken}/>
-    </div>
-  );
+    const protectedViews = () => {
+        return (sessionToken === localStorage.getItem("token") ? <WorkoutIndex token={sessionToken} />
+        : <Auth updateToken={updateToken}/>)
+    }
+
+    return (
+        <div>
+            <Sitebar clickLogout={clearToken}/>
+            {protectedViews()}
+        </div>
+    );
 }
 
 export default App;
--- src/home/Navbar.js
+++ src/home/Navbar.js
@@ -1,15 +1,34 @@
-import React from "react";
-import { 
+import React, {useState} from 'react';
+import {
     Navbar,
-    NavbarBrand
-} from "reactstrap";
+    NavbarBrand,
+    Collapse,
+    NavbarToggler,
+    Nav,
+    NavItem,
+    Button
+} from 'reactstrap';
 
 const Sitebar = (props) => {
-    return(
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => {
+        let newIsOpen = !isOpen;
+        setIsOpen(newIsOpen);
+    }
+
+    return (
         <Navbar color="faded" light expand="md">
-            <NavbarBrand href="/">Workout Log</NavbarBrand>
+        <NavbarBrand href="/">Workout Log</NavbarBrand>
+        <NavbarToggler onClick={toggle}/>
+        <Collapse isOpen={isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+                <NavItem>
+                    <Button onClick={props.clickLogout}>Logout</Button>
+                </NavItem>
+            </Nav>
+        </Collapse>
         </Navbar>
     )
-}
+ }
 
 export default Sitebar;
